99a80432698aa99d15094344ad29dd5326fa475c
IgnacioToledoDev <igna.toledot@gmail.com>
2023-05-08 22:04:19 -0400

    Update config database and app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ import bodyParser from 'body-parser';
 import { router } from './routes/index';
 import db from './config/database'
 
-
 /**GLOBALS CONFIGS */
 const app = express();
 
@@ -15,18 +14,13 @@ dotenv.config();
 const PORT = process.env.PORT || 4200;
 
 /**DATABASE */
-db().then(() => console.log('Connexion ready')).catch((err) => console.log(`Oh a ocurrido un error ${err}`));
-
+db()
+    .then(() => console.log('Connexion ready'))
+    .catch((err) => console.log(`Oh a ocurrido un error ${err}`));
 
 /**App use */
 app.use(bodyParser.json());
 app.use(cors());
 app.use(router)
-
-
-
-
-
-
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,9 @@ import { connect } from "mongoose";
 dotenv.config();
 
 /**Connection to database */
+/**
+ * @description Connect with mongo database
+ */
 async function dbConnect(): Promise<void> {
     const DB_URI = <string>process.env.DB_URI;
     await connect(DB_URI);
